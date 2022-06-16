4e1453cd7ce5289aad940ec691d1f28720caf92c
KimChanHo <rlacksgh0826@gmail.com>
2022-06-16 09:39:47 +0900

    기본적인 기능 개발

--- src/component/App.js
+++ src/component/App.js
@@ -6,6 +6,8 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import CssBaseline from '@mui/material/CssBaseline';
 import axios from "axios/index";
+import HistoryTable from "./HistoryTable";
+import Variable from "./Variable";
 
 class App extends React.Component {
     constructor(props) {
@@ -13,63 +15,57 @@ class App extends React.Component {
 
         this.state = {
             startDate: '',
-            endEate: '',
+            endDate: '',
             userName: '',
             adAccount: '',
-            getList: [{}]
+            detail: {},
+            loading: false,
+            historyList: []
         }
     }
 
     componentDidMount() {
-       this.getHistory();
+       /**/
     }
 
-    callApiGet = (url, params) => {
-        return axios.get(url, {
+    callApiGet = async (url, params) => {
+        const response = await axios.get(url, {
                 params: params,
                 headers: {
-                    Authorization: 'Basic ',
+                    Authorization: 'Basic ' + Variable.Authorization,
                     'Content-type': 'application/json'
-                    }
                 }
-            )
-            .then(function (response) {
-                return response.data;
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-    }
-
-    callApiPost = (url, params) => {
-        return axios.post(url, params, {
-            config: {
-                headers: {
-                    'Authorization': 'Basic ',
-                    'Content-Type': 'application/json'
-                }
-            }
-        })
-        .then(function (response) {
-            return response.data;
-        })
-        .catch(function (error) {
-            console.log('callApiPost fail!!', error);
-        });
-    }
-
-    getHistory = async () => {
-        const url = '/rest/api/2/search';
-        const getList = await this.callApiGet(url, {
-                "jql":" project=NGCPO AND due >= 2022-04-01 AND due <= 2022-05-30 AND assignee in (Kim.ChanHo01) order by updated DESC", "maxResults":200
             }
         );
 
-        this.setState({
-            getList: getList
-        })
+        return response.data;
+    }
 
-        // return getList;
+    getHistory = async () => {
+        const url = Variable.ProxyUrl + Variable.JiraUrl + Variable.RestSearchUrl;
+        let getList = await this.callApiGet(url, {
+                "jql":`project=NGCPO AND due >= ${this.state.startDate} AND due <= ${this.state.endDate} AND assignee in (${this.state.adAccount}) order by updated DESC`, "maxResults":200
+            }
+        )
+
+        const getListEih = await this.callApiGet(url, {
+                "jql":`project=EIH AND resolved >= ${this.state.startDate} AND resolved <= ${this.state.endDate} AND watcher in (${this.state.adAccount}) order by updated DESC`, "maxResults":200
+            }
+        )
+
+        const mergeList = getList['issues'].concat(getListEih['issues']);
+
+        this.setState({
+            historyList: mergeList,
+            loading: false
+        });
+    }
+
+    renderLoading = () => {
+        console.log('renderLoading :: ', this.state.loading)
+        this.setState({
+           loading: true
+        });
     }
 
     handleTag = (event) => {
@@ -85,29 +81,21 @@ class App extends React.Component {
     handleSubmit = (event) => {
         event.preventDefault();
 
-        console.log('state data :::: ', this.state);
-    }
-
-    // handleSubmit = (event) => {
-    //     event.preventDefault();
-    //     const data = new FormData(event.currentTarget);
-    //     console.log({
-    //         email: data.get('email'),
-    //         password: data.get('password'),
-    //     });
-    // };
+        this.renderLoading();
+        this.getHistory();
+    };
 
     render() {
         return(
-            <div>
             <Box
                 component="form"
+                onSubmit={this.handleSubmit}
                 sx={{
                     display: 'flex',
                     flexDirection: 'column',
                     alignItems: 'center',
                     mt: 5,
-                    '& .MuiTextField-root': { m: 1, width: '25ch' },
+                    '& .MuiTextField-root': { m: 1, width: '40ch' },
                 }}
                 noValidate
             >
@@ -126,14 +114,15 @@ class App extends React.Component {
                         id="startDate"
                         name="startDate"
                         label="기한 시작일시"
-                        helperText="월간보고 시작 기준일"
+                        helperText="월간보고 시작 기준일(ex: 2022-01-01)"
                         onChange={this.handleTag.bind(this)}
                     />
                     <TextField
                         id="endDate"
                         name="endDate"
                         label="기한 종료일시"
-                        helperText="월간보고 종료 기준일"
+                        helperText="월간보고 종료 기준일(ex: 2022-12-31)"
+                        onChange={this.handleTag.bind(this)}
                     />
                 </Box>
                 <Divider />
@@ -146,13 +135,15 @@ class App extends React.Component {
                         id="userName"
                         name="userName"
                         label="사용자명"
-                        helperText="노트북 로그인계정의 등록 소유자명"
+                        helperText="노트북 로그인계정의 등록 소유자명(ex: 홍길동)"
+                        onChange={this.handleTag.bind(this)}
                     />
                     <TextField
                         id="adAccount"
                         name="adAccount"
                         label="AD 계정"
-                        helperText="노트북 로그인 계정"
+                        helperText="노트북 로그인 계정(ex: Hong.Kildong)"
+                        onChange={this.handleTag.bind(this)}
                     />
                 </Box>
                 <Box
@@ -169,11 +160,21 @@ class App extends React.Component {
                         조회
                     </Button>
                 </Box>
-
-
+                <Box sx={{
+                    marginTop: 1,
+                    marginBottom: 10
+                }}>
+                    {
+                            this.state.loading ?
+                                'Loading..'
+                                :
+                                this.state.historyList.length > 0 ?
+                                    <HistoryTable historyList={this.state.historyList} />
+                                    :
+                                    '조회된 내역이 없습니다.'
+                    }
+                </Box>
             </Box>
-                {this.state.getList}
-            </div>
         )
     }
 }
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,10 +1,12 @@
 // import { createProxyMiddleware } from "http-proxy-middleware"; //이렇게 하면 안되고
+import Variable from "./component/Variable";
 const { createProxyMiddleware } = require("http-proxy-middleware"); // 이렇게 하면 됨 무슨차이지?
+
 module.exports = function(app) {
     app.use(
-        '/rest/api/2/search',
+        Variable.RestSearchUrl,
         createProxyMiddleware({
-            target: '',
+            target: Variable.JiraUrl,
             changeOrigin: true,
         })
     );
